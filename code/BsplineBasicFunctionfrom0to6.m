4cb3eabd8f53282e6f93fdd18eb9a601787ec136
Elmira Mirzabeigi <104006048+ElmiraMirzabeigi@users.noreply.github.com>
2024-10-05 13:43:46 +0330

    Add files via upload

---
+++ code/BsplineBasicFunctionfrom0to6.m
@@ -0,0 +1,19 @@
+function[Bs]=BsplineBasicFunctionfrom0to6
+Bs=cell(1,9);
+k=0;
+for r=1:9
+    if r~=1 && r~=2
+       t=0.25+k:0.5:1.75+k;
+       Bs{1,r}=spmak(t,1);
+       fnplt(Bs{1,r})
+       hold on;
+       k=k+0.5;
+    else
+       t=0+k:0.75:2.25+k;
+       Bs{1,r}=spmak(t,1);
+       fnplt(Bs{1,r})
+       hold on;
+       k=k+0.75; 
+    end
+end
+end
